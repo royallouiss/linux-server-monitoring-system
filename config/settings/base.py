11cd395168d919bb476e1baa940f202e77273685
Royal Louis <royallouis1003@gmail.com>
2026-08-29 09:26:26 +0530

    feat: initialize monitoring app

--- config/settings/base.py
+++ config/settings/base.py
@@ -104,6 +104,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "linux_server_monitoring_system.users",
     "linux_server_monitoring_system.servers",
+    "linux_server_monitoring_system.monitoring",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
