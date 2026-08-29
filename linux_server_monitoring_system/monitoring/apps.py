9addeaeae0f46916024cf51b45eafd84634ed938
Royal Louis <royallouis1003@gmail.com>
2026-08-29 10:38:42 +0530

    feat: add memory metrics collector

--- linux_server_monitoring_system/monitoring/apps.py
+++ linux_server_monitoring_system/monitoring/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class MonitoringConfig(AppConfig):
-    name = 'monitoring'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "linux_server_monitoring_system.monitoring"
