5e663bdf5e0fdf68568c3a7dd853a6a0acfe9e87
Meha <mehasamuvel@gmail.com>
2026-09-14 17:56:47 +0530

    Add REST API routes

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
+
 from linux_server_monitoring_system.monitoring.api.views import ServerViewSet
 from linux_server_monitoring_system.users.api.views import UserViewSet
 
@@ -8,7 +9,6 @@ router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
 router.register("servers", ServerViewSet, basename="server")
-
 
 app_name = "api"
 urlpatterns = router.urls
